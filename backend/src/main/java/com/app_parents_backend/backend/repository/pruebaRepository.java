add10a4236b8d24caff091740057e4a184e98036
Carlos-300 <megat300@hotmail.com>
2025-10-09 15:44:34 -0300

    Add: base de datos con postgresql (insert y create)

---
+++ backend/src/main/java/com/app_parents_backend/backend/repository/pruebaRepository.java
@@ -0,0 +1,8 @@
+package com.app_parents_backend.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app_parents_backend.backend.model.prueba;
+
+public interface pruebaRepository extends JpaRepository<prueba, Long> {
+}
