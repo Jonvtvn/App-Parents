8a812d11cdb5187da2fd1f0453059b254539cfea
Carlos-300 <megat300@hotmail.com>
2025-10-17 15:08:12 -0300

    save

--- backend/src/main/java/com/app_parents_backend/backend/config/JwtUtil.java
+++ backend/src/main/java/com/app_parents_backend/backend/config/JwtUtil.java
@@ -10,7 +10,7 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "clave_super_larga_y_segura_para_firmar_token_12345";
+    private final String SECRET_KEY = "4/YQusX7FkjA0jZNgvd0Ph0pyZ7/Wpd3DfZXg85mhGuI5D/w+O6FbHyQwOhlqzW7xl3KB9uYaESeUlpaabGUhQ==";
     private final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hora
 
     private Key getSigningKey() {
